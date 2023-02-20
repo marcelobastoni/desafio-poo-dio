36708b540050b6059f5ba1da6ca3b6eca3e627d9
marcelobastoni <marcelobastoni@gmail.com>
2023-02-20 17:11:06 -0300

    Implemetação dos Métodos inscreverBootcamp, progredir e calcularXp.

--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -2,19 +2,35 @@ package br.com.dio.desafio.dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
-
     private String nome;
     private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
     private Set<Conteudo> conteudoConcluidos = new LinkedHashSet<>();
 
-    public void inscreverBootcamp(Bootcamp bootcamp){}
+    public void inscreverBootcamp(Bootcamp bootcamp){
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
+    }
 
-    public void progredir() {}
+    public void progredir() {
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if (conteudo.isPresent()){
+            this.conteudoConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        }else{
+                System.err.println("Você não está matriculado em nenhum contéudo!");
+        }
+    }
 
-    public void calcularXp() {}
+    public double calcularXp() {
+        return this.conteudoConcluidos
+                .stream()
+                .mapToDouble(Conteudo :: calcularXp)
+                .sum();
+    }
 
     public String getNome() {
         return nome;
